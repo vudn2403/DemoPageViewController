4a71991ed806105a8eff8da3d31456e94a0f5608
Đinh Ngọc Vũ <vudn.it@gmail.com>
2018-10-03 15:08:39 +0700

    init

--- Pods/Target Support Files/Pods-DemoPageViewController/Pods-DemoPageViewController-frameworks.sh
+++ Pods/Target Support Files/Pods-DemoPageViewController/Pods-DemoPageViewController-frameworks.sh
@@ -144,9 +144,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DLRadioButton/DLRadioButton.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/DLRadioButton/DLRadioButton.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MaterialComponents/MaterialComponents.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
